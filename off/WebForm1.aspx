4ec2ba751edd60e919f16eaa7f8220861771543e
Hazal Deniz <hzl-deniz@hotmail.com>
2020-05-25 14:27:12 +0300

    add login page

---
+++ off/WebForm1.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="off.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+
+        body{
+            background:#1d56ec;
+        }
+
+        .formclass
+
+        {
+            padding:20px;
+            margin:0px auto;
+            background:#fff;
+            width:200px;
+        }
+
+        h2 {
+            text-align:center;
+        }
+
+        .input {
+
+            padding:2px;
+            
+
+            width:100%;
+        }
+
+
+
+
+
+
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="formclass">
+            <h2>GİRİŞ SAYFASI</h2>
+
+            <asp:TextBox ID="txtuser" CssClass="input" placeholder="Kullanıcı Adı" runat="server"></asp:TextBox><br /><br />
+            <asp:TextBox ID="txtpass" CssClass="input" placeholder="Parola" TextMode="Password" runat="server"></asp:TextBox><br /><br />
+
+            <asp:Button ID="btnlogin" CssClass="input" width="100%" runat="server" Text="GİRİŞ" OnClick="btnlogin_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ off/WebForm1.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace off
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session["user"] != null)
+            {
+                Response.Redirect("/universite/universite_listesi.aspx");
+            }
+
+        }
+
+        protected void btnlogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connect"].ToString());
+            con.Open();
+            string query = "select count(*) from tbluser where user_name='"+txtuser.Text+"'and pass='"+txtpass.Text+"'";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            string output = cmd.ExecuteScalar().ToString();
+
+            if(output == "1")
+            {
+                Session["user"] = txtuser.Text;
+                Response.Redirect("/universite/universite_listesi.aspx");
+            }
+            else
+            {
+                Response.Write("Giriş Başarısız!");
+            }
+        }
+    }
+}
